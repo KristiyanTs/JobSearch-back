c89617bf5cdd29963a007b205c6bf53915a887e2
kristiyan <kristiyan.c@gmail.com>
2019-05-24 09:44:20 +0300

    Deploy configuration

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM ruby:2.5.1
-WORKDIR /srv/infbase
-ADD Gemfile* /srv/infbase/
-RUN bundle install
-RUN gem install foreman
-ADD . .
-CMD foreman start
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-server '206.189.126.197', user: 'deploy', roles: %w{app db web}
+server '209.97.137.207', user: 'deploy', roles: %w{app db web}
 
 
 
