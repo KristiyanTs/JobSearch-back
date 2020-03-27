7a99592fd8d6f762525eda2b76da3421a3bb678d
Kristiyan96 <kristiyan.c@gmail.com>
2020-03-27 20:55:13 +0200

    Config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,13 +9,23 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080'
-    resource(
-      '*',
-      headers: :any,
-      expose: ["Authorization"],
-      credentials: true,
-      methods: %i[get post put patch delete options head]
-    )
+    # origins 'http://localhost:8080'
+    # resource(
+    #   '*',
+    #   headers: :any,
+    #   expose: ["Authorization"],
+    #   credentials: true,
+    #   methods: %i[get post put patch delete options head]
+    # )
+
+    allow do
+      origins 'http://localhost:8080'
+  
+      resource '*',
+               headers: :any,
+               expose: ["Authorization"],
+               credentials: true,
+               methods: %i[get post put patch delete options head]
+    end
   end
 end
