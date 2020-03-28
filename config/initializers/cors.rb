2be68c68da6b8e8de32e91bd579c443eb4bd2105
Kristiyan96 <kristiyan.c@gmail.com>
2020-03-28 12:43:08 +0200

    Config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -18,7 +18,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     #   methods: %i[get post put patch delete options head]
     # )
 
-    origins 'http://localhost:8080'
+    origins 'https://bgmentor.com'
 
     resource '*',
               headers: :any,
