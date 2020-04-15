364050d413ba5fd56ce8f1319442680b5d023478
Kristiyan96 <kristiyan.c@gmail.com>
2020-04-15 04:18:52 +0300

    Delete a group

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,6 +4,7 @@ class Group < ApplicationRecord
   belongs_to :location, optional: true
   
   has_many :memberships, dependent: :destroy
+  has_many :lessons, dependent: :destroy
 
   enum lesson: [:individual, :collective]
   
